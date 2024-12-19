d50507adb1a3d460392e59b61c4118cd803c5360
Arumugasamy <akilthiru2000@gmail.com>
2024-12-19 11:54:43 +0530

    comments chnage

--- Backend/email.js
+++ Backend/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-  // 1) Create a transporter
+  // 1 Create a transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
@@ -11,7 +11,7 @@ const sendEmail = async (options) => {
     },
   });
 
-  // 2) Define the email options
+  // 2 email options
   const mailOptions = {
     from: "Admin <admin@codem.com>",
     to: options.email,
@@ -20,7 +20,7 @@ const sendEmail = async (options) => {
     // html:
   };
 
-  // 3) Actually send the email
+  // 3 send the email
   await transporter.sendMail(mailOptions);
 };
 
